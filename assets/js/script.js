9dfa25fd969bcc61d2a313820ba447fee8efd170
Celine_lhp5 <celine.louvel.76@outlook.fr>
2020-11-02 16:25:26 +0100

    modif ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-// let day = 6;
 let day = prompt('Saisissez un nombre entre 1 et 7 :');
 
 switch (parseInt(day)) {
@@ -28,3 +27,35 @@ switch (parseInt(day)) {
         alert('Ce n\'est pas un jour de la semaine');
         break;
 }
+
+// ou...
+
+// let day = 6;
+
+// switch (day) {
+//     case 1:
+//         alert('Lundi');
+//         break;
+//     case 2:
+//         alert('Mardi');
+//         break;
+//     case 3:
+//         alert('Mercredi');
+//         break;
+//     case 4:
+//         alert('Jeudi');
+//         break;
+//     case 5:
+//         alert('Vendredi');
+//         break;
+//     case 6:
+//         alert('Samedi');
+//         break;
+//     case 7:
+//         alert('Dimanche');
+//         break;
+
+//     default:
+//         alert('Ce n\'est pas un jour de la semaine');
+//         break;
+// }
